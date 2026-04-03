a522d7cbdc8e691fe8f8afa027da9f422f38dba3
jam06452 <jam@jam06452.uk>
2026-04-03 14:31:03 +0100

    Changes to make this build inside the correct elixir folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../priv/static',
+    emptyOutDir: true,
+    assetsDir: 'js_assets',
+    manifest: true
+  },
+  base: '/'
 })
